cb7a86c97d15333acc22b4ee504ee2926dc65e5d
Shelly Grossman <shelly@certora.com>
2025-10-26 19:47:48 +0200

    changes that made the RAG work in MacOS M1

--- scripts/ragbuild.py
+++ scripts/ragbuild.py
@@ -22,7 +22,7 @@ import contextvars
 import pathlib
 import sys
 
-verisafe_dir = str(pathlib.Path(__file__).parent.parent.parent.absolute())
+verisafe_dir = str(pathlib.Path(__file__).parent.parent.absolute())
 
 if verisafe_dir not in sys.path:
     sys.path.append(verisafe_dir)
