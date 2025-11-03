c0751e826c86e0682e31d87a5fb470f67c3fd1c1
jtoman <jtoman@users.noreply.github.com>
2025-11-03 09:42:26 -0800

    Folds CEX Tool into verisafe repo (#15)
    * Fold cex analyzer back into this repo
    
    * fixes

--- verisafe/prover/ptypes.py
+++ verisafe/prover/ptypes.py
@@ -1,7 +1,55 @@
-from typing import Literal, Optional
+from typing import Literal, Optional, TypeVar
 from dataclasses import dataclass
 
 StatusCodes = Literal["VERIFIED", "VIOLATED", "TIMEOUT", "ERROR", "SANITY_FAIL"]
+
+class _Missing:
+    pass
+
+_MISSING = _Missing()
+
+_T = TypeVar('_T')
+
+def _default_or(
+    curr: _T,
+    update: _Missing | _T
+) -> _T:
+    if isinstance(update, _Missing):
+        return curr
+    else:
+        return update
+
+@dataclass
+class RulePath:
+    rule: str
+    contract: Optional[str] = None
+    method: Optional[str] = None
+    sanity: bool = False
+
+    def copy(
+            self,
+            rule : str | _Missing = _MISSING,
+            contract : str | None | _Missing = _MISSING,
+            method : str | None | _Missing = _MISSING,
+            sanity : bool | _Missing = _MISSING
+    ) -> 'RulePath':
+        return RulePath(
+            rule=_default_or(self.rule, rule),
+            contract=_default_or(self.contract, contract),
+            method=_default_or(self.method, method),
+            sanity=_default_or(self.sanity, sanity)
+        )
+    def pprint(self) -> str:
+        if self.contract is not None:
+            if self.method is None:
+                return f"{self.rule} in contract {self.contract}"
+
+        if self.method is not None:
+            return f"{self.rule} for {self.method}"
+        else:
+            return self.rule
+
+
 
 @dataclass
 class RuleResult:
@@ -11,6 +59,10 @@ class RuleResult:
     If status == VIOLATED, then cex_dump is non-null, and will contain the XML representation
     of the CEX
     """
-    name: str
+    path: RulePath
     cex_dump: Optional[str]
     status: StatusCodes
+
+    @property
+    def name(self) -> str:
+        return self.path.pprint()
