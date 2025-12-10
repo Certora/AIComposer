56589d805e299f71553892e43e156a2c54e55081
Omer Lerinman <omer.l@certora.com>
2025-12-10 14:20:29 +0200

    Fix: Clarify contract target in trivial example files

--- examples/trivial/Intf.sol
+++ examples/trivial/Intf.sol
@@ -1,2 +1,7 @@
 interface Intf {
+    /**
+     * @dev Returns a number that should be divisible by 4 and 5
+     * @return The result of the magic function
+     */
+    function the_magic_function() external returns (uint256);
 }
