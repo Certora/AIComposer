5a80bd7d322e6fcb1ea759e78db524c7b0caad80
John Toman <jtoman@cs.washington.edu>
2026-05-04 10:54:51 -0700

    More test harness changes
    Live scenario, new tape

---
+++ composer/testing/scenarios/autoprove_counter/src/Counter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.29;
+
+contract Counter {
+    uint256 public count;
+    mapping(address => uint256) public increments;
+
+    function increment() external {
+        require(msg.sender != address(0));
+        count += 1;
+        increments[msg.sender] += 1;
+    }
+}
