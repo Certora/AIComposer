d0368e321f42b5d7bc3ed3fc77b7e09f5054b39b
John Toman <jtoman@cs.washington.edu>
2026-05-22 12:06:28 -0700

    Thread logging
    Adds a context manager which will intercept all graph call that occur
    through the `graph_runner` and log salient information:
    * the description
    * the "run id"
    * the thread id
    * the tool id spawning this call
    * start/end times
    * start checkpoint (if any)
    * end checkpoit
    
    This also introduces the concept of a "run id"; these are globally
    unique for a run of the autoprover.
    
    A good question is why we can't treat thread ids as uniquely identifying
    a *run* of that thread id. It is plausible (and quite likely) that we
    will want to support resuming an interrupted thread id at some point
    (due to a failure of, e.g., the anthropic API). Thus, multiple *runs* of
    the same thread id can occur. Thus, we associate those each *run* of the thread
    id with the overall run id (which uniquely identifies the overall
    autoprove run)

---
+++ composer/io/thread_logging.py
@@ -0,0 +1,154 @@
+from typing import TypedDict, Protocol, AsyncIterator, Any
+from dataclasses import dataclass
+from datetime import datetime, UTC
+from uuid import uuid4
+from contextlib import asynccontextmanager
+from contextvars import ContextVar
+
+from langchain_core.runnables import RunnableConfig
+from langgraph.store.base import BaseStore
+
+class _WithTimings(TypedDict):
+    start_time: str
+    end_time: str | None
+
+class RunMeta(_WithTimings):
+    tags: dict[str, Any]
+
+class ThreadMeta(_WithTimings):
+    run_id: str
+    thread_id: str
+    start_time: str
+    description: str
+    from_tool_id: str | None
+
+    end_time: str | None
+
+    start_checkpoint_id: str | None
+    end_checkpoint_id: str | None
+
+META_NS = ("logging",)
+
+def _time_string() -> str:
+    return datetime.now(UTC).isoformat()
+
+class CheckpointLogger(Protocol):
+    def last_checkpoint(self, last_checkpoint: str) -> None:
+        ...
+
+@dataclass
+class _ThreadRunHandle:
+    _store: BaseStore
+    _partial: ThreadMeta
+    _key: str
+    _ns: tuple[str, ...]
+    _last_checkpoint: str | None
+
+    def last_checkpoint(self, last_checkpoint: str):
+        self._last_checkpoint = last_checkpoint
+
+    async def complete(
+        self,
+    ):
+        end_time = _time_string()
+        to_write : ThreadMeta = {
+            **self._partial,
+            "end_time": end_time,
+            "end_checkpoint_id": self._last_checkpoint
+        }
+        await self._store.aput(self._ns, self._key, {**to_write})
+
+class ThreadLogger:
+    def __init__(self, store: BaseStore, run_id: str, ns: tuple[str, ...]):
+        self.store = store
+        self.ns = ns
+        self.run_id = run_id
+
+    async def start(
+        self, thread_id: str, start_checkpoint: str | None, from_tool_id: str | None, description: str
+    ) -> _ThreadRunHandle:
+        start_time = _time_string()
+        thread_run_id = uuid4().hex
+        staged : ThreadMeta = {
+            "start_time": start_time,
+            "from_tool_id": from_tool_id,
+            "end_checkpoint_id": None,
+            "end_time": None,
+            "run_id": self.run_id,
+            "start_checkpoint_id": start_checkpoint,
+            "thread_id": thread_id,
+            "description": description
+        }
+        await self.store.aput(
+            self.ns, thread_run_id, {**staged}
+        )
+        return _ThreadRunHandle(
+            _store=self.store,
+            _partial=staged,
+            _key=thread_run_id,
+            _ns=self.ns,
+            _last_checkpoint=None
+        )
+
+_logger : ContextVar[None | ThreadLogger] = ContextVar("_logger", default=None)
+
+@asynccontextmanager
+async def log_thread(
+    description: str,
+    runnable: RunnableConfig,
+    within_tool: str | None
+) -> AsyncIterator[CheckpointLogger]:
+    cur = _logger.get()
+    if not cur:
+        class Dummy:
+            def last_checkpoint(self, last_checkpoint: str):
+                pass
+        yield Dummy()
+        return
+    assert "configurable" in runnable
+
+    thread_id = runnable["configurable"]["thread_id"]
+    
+    checkpoint_id = runnable["configurable"].get("checkpoint_id", None)
+
+    handle = await cur.start(
+        description=description,
+        from_tool_id=within_tool,
+        start_checkpoint=checkpoint_id,
+        thread_id=thread_id
+    )
+    try:
+        yield handle
+    finally:
+        try:
+            await handle.complete()
+        except:
+            pass # swallow
+
+@asynccontextmanager
+async def thread_logger(
+    store: BaseStore,
+    tags: dict[str, Any],
+    ns: tuple[str, ...] = META_NS
+) -> AsyncIterator[None]:
+    run_id = uuid4().hex
+
+    run_meta : RunMeta = {
+        "start_time": _time_string(),
+        "tags": tags,
+        "end_time": None
+    }
+    run_ns = ns + ("runs",)
+    await store.aput(run_ns, run_id, {**run_meta})
+    tok = _logger.set(ThreadLogger(
+        store, run_id, ns + ("threads",)
+    ))
+    try:
+        yield
+    finally:
+        _logger.reset(tok)
+        run_meta["end_time"] = _time_string()
+        try:
+            await store.aput(run_ns, run_id, {**run_meta})
+        except:
+            pass
