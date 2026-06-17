7132c177ff3e0db3adcec45b8ec72b2e99412f19
John Toman <jtoman@cs.washington.edu>
2026-06-17 12:57:29 -0700

    oops

--- composer/prover/core.py
+++ composer/prover/core.py
@@ -61,8 +61,7 @@ def make_prover_options(*, cloud: bool) -> ProverOptions:
     if cloud:
         extras = [
             "--global_timeout", str(int(DEFAULT_GLOBAL_TIMEOUT)),
-            "--server", cloud_server_for_env(),
-            "--prover_version", "master"
+            "--server", cloud_server_for_env()
         ]
     return ProverOptions(extra_args=extras)
 
